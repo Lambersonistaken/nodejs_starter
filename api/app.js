11f32fe33ea5f9dc2db6c24ac1ee34bb95743b58
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-01-11 18:32:21 +0300

    update

--- api/app.js
+++ api/app.js
@@ -18,6 +18,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  console.log("Ben app.js ile tanımlanan bir middleware'im.");
+  next();
+});
+
 app.use("/api", require("./routes/index"));
 // app.use("/users", require("./routes/users"));
 // app.use("/auditlogs", require("./routes/auditlogs"));
--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,9 +1,22 @@
 var express = require("express");
 var router = express.Router();
 
+const isAuthenticated = true;
+
+router.all("*", (req, res, next) => {
+  if (isAuthenticated) {
+    next();
+  } else {
+    res.status(401).json({
+      message: "Unauthorized",
+      error: "Not Authenticated",
+    });
+  }
+});
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  res.send("respond with a success");
 });
 
 module.exports = router;
