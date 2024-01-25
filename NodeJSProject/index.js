ad6115dc3de33b6e0d5de731c84c073488f3cf5f
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-01-25 20:33:59 +0300

    dinamik link and other stuff

--- NodeJSProject/index.js
+++ NodeJSProject/index.js
@@ -2,18 +2,28 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const data = [
+  { id: 1, name: "Samsung S6", price: 3000, isActive: true },
+  { id: 2, name: "Samsung S7", price: 4000, isActive: true },
+  { id: 3, name: "Samsung S8", price: 5000, isActive: true },
+];
+
+app.set("view engine", "ejs"); // view engine olarak ejs kullanılacak
+
 // middleware routing
 app.use("/products/:id", (req, res) => {
-  res.send(`products detail for id ${req.params.id}`);
+  res.render("products_details");
 });
 
 app.use("/products", (req, res) => {
   // nodejs routing yapısı
-  res.send("products");
+  res.render("products", {
+    products: data,
+  });
 });
 
 app.use("/", (req, res) => {
-  res.send("ana sayfa");
+  res.render("index");
 });
 
 app.listen(port, () => {
