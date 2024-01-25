c7142e523f409e0f28aa0b6f88d3240da3f31d55
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-01-25 16:08:59 +0300

    node basic

--- NodeJSProject/index.js
+++ NodeJSProject/index.js
@@ -1,12 +1,30 @@
 // request - response  => server - client
 
 var http = require("http"); // http module
+var fs = require("fs"); // file system module
 
 var server = http.createServer((req, res) => {
   console.log("request was made: " + req.url); // request url
 
-  res.write("<h1> Selamun Aleykum </h1>"); // response write
-  res.end(); // response end
+  if (req.url == "/") {
+    fs.readFile("index.html", (err, html) => {
+      // burada ilk parametre hata varsa diye ikinci parametre ise html dosyası yani data
+      res.write(html); // response write
+      res.end(); // response end
+    });
+  } else if (req.url == "/urunler") {
+    fs.readFile("ürünler.html", (err, html) => {
+      // burada ilk parametre hata varsa diye ikinci parametre ise html dosyası yani data
+      res.write(html); // response write
+      res.end(); // response end
+    });
+  } else {
+    fs.readFile("404.html", (err, html) => {
+      // burada ilk parametre hata varsa diye ikinci parametre ise html dosyası yani data
+      res.write(html); // response write
+      res.end(); // response end
+    });
+  }
 }); // create server fonksiyonu node içinde var
 
 server.listen(3000, () => {
