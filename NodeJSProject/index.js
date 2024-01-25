a6d06ce56af513ff21676ca1a122b733f9e6ddd5
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-01-25 16:20:14 +0300

    middleware routing

--- NodeJSProject/index.js
+++ NodeJSProject/index.js
@@ -1,32 +1,21 @@
-// request - response  => server - client
+const express = require("express");
+const app = express();
+const port = 3000;
 
-var http = require("http"); // http module
-var fs = require("fs"); // file system module
+// middleware routing
+app.use("/products/:id", (req, res) => {
+  res.send(`products detail for id ${req.params.id}`);
+});
 
-var server = http.createServer((req, res) => {
-  console.log("request was made: " + req.url); // request url
+app.use("/products", (req, res) => {
+  // nodejs routing yapısı
+  res.send("products");
+});
 
-  if (req.url == "/") {
-    fs.readFile("index.html", (err, html) => {
-      // burada ilk parametre hata varsa diye ikinci parametre ise html dosyası yani data
-      res.write(html); // response write
-      res.end(); // response end
-    });
-  } else if (req.url == "/urunler") {
-    fs.readFile("ürünler.html", (err, html) => {
-      // burada ilk parametre hata varsa diye ikinci parametre ise html dosyası yani data
-      res.write(html); // response write
-      res.end(); // response end
-    });
-  } else {
-    fs.readFile("404.html", (err, html) => {
-      // burada ilk parametre hata varsa diye ikinci parametre ise html dosyası yani data
-      res.write(html); // response write
-      res.end(); // response end
-    });
-  }
-}); // create server fonksiyonu node içinde var
+app.use("/", (req, res) => {
+  res.send("ana sayfa");
+});
 
-server.listen(3000, () => {
-  console.log("server is listening on port 3000");
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
